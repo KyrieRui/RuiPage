5b98d211ec6a9dd5ff21257d6e15a08b57ef6118
KyrieRui <kyrie19960324@gmail.com>
2024-02-27 10:42:16 +1300

    text added

--- src/App.jsx
+++ src/App.jsx
@@ -11,22 +11,22 @@ function App() {
       <header className="px-4 py-6">
         <div className="container m-auto flex justify-between">
           <div>
-            <h1 className="text-5xl font-semiblod">Ray</h1>
+            <h1 className="text-5xl font-mono">Ray</h1>
           </div>
-          <div>
+          <div className="">
             <ul className="flex gap-4">
               <li>
-                <a className="text-4xl text-gray-500 hover:text-black cursor-pointer">
+                <a className="font-mono text-4xl text-gray-500 hover:text-black cursor-pointer">
                   Works
                 </a>
               </li>
               <li>
-                <a className="text-4xl text-gray-500 hover:text-black cursor-pointer">
+                <a className="font-mono text-4xl text-gray-500 hover:text-black cursor-pointer">
                   Blogs
                 </a>
               </li>
               <li>
-                <a className="text-4xl text-gray-500 hover:text-black cursor-pointer">
+                <a className="font-mono text-4xl text-gray-500 hover:text-black cursor-pointer">
                   Contant
                 </a>
               </li>
@@ -37,12 +37,15 @@ function App() {
       <main className="px-4 py-32">
         <div className="container m-auto flex justify-between items-center">
           <div className="w-1/2">
-            <h2 className="text-4xl font-bold">Hi, I'm Ray</h2>
-            <h2 className="text-4xl font-bold">I am a Junior developer</h2>
-            <p className="mt-3 text-gray-500">
-              I just received my BSC from Victoria University of Wellington past
-              November 2023!
-            </p>
+            <div className="shadow-lg">
+              <h2 className="text-4xl font-bold">Hi, I'm Ray</h2>
+              <h2 className="text-4xl font-bold">I am a Junior developer</h2>
+              <p className="mt-3 text-gray-500">
+                Graduated from Victoria University of Wellington November 2023
+              </p>
+              <p className="mt-3 text-gray-500">Majoring in Computer Science</p>
+            </div>
+
             <div className="container m-auto flex gap-8 justify-center">
               <button className="px-5 py-3 bg-blue-500 rounded-md text-white mt-5">
                 Download my CV
@@ -54,6 +57,7 @@ function App() {
           </div>
           <div className="w-2/2">
             <img
+              className="rounded-full"
               src="https://scontent-syd2-1.xx.fbcdn.net/v/t39.30808-6/328998714_885168419257223_3222668373957364843_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=efb6e6&_nc_ohc=HNiHZXgSB1UAX8-rnks&_nc_ht=scontent-syd2-1.xx&oh=00_AfAjAoLlgpXdGbd8OeAs3SZieSBBdGqklVa_02hrYbJvuw&oe=65DFF051"
               alt="Rui"
               style={{ width: "400px", height: "auto" }}
@@ -72,37 +76,38 @@ function App() {
         </div>
         <div className="flex justify-between mt-5 gap-3">
           <div className="bg-white p-3 rounded-sm basis-1/2">
-            <h2 className="text-lg font-semibold">
-              Making a Design perfection
-            </h2>
+            <h2 className="text-lg font-semibold">Google Gemma first try</h2>
             <div className="mt-3 flex gap-3 items-center">
               <span className="text-gray-400 text-sm font-light">
-                26 Feb 2024
+                22 Feb 2024
               </span>
               <span className="w-[1px] h-[17px] bg-gray-700"></span>
               <span className="text-gray-400 text-sm font-light">
-                Design, Pattern
+                Hugging Face, Python
               </span>
             </div>
             <p className="mt-3 text-base font-light">
-              sum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod
+              Google has released Gemma at Hugging Face. Now developers can use
+              it to do more things like RAG or fine tunning LLM. The blog is a
+              simple set up tutorial.
             </p>
           </div>
           <div className="bg-white p-3 rounded-sm basis-1/2">
-            <h2 className="text-lg font-semibold">
-              Making a Design perfection
-            </h2>
+            <h2 className="text-lg font-semibold">YOLO V5 on Android phone</h2>
             <div className="mt-3 flex gap-3 items-center">
               <span className="text-gray-400 text-sm font-light">
-                26 Feb 2024
+                26 Jan 2024
               </span>
               <span className="w-[1px] h-[17px] bg-gray-700"></span>
               <span className="text-gray-400 text-sm font-light">
-                Design, Pattern
+                Yolov5, Android
               </span>
             </div>
             <p className="mt-3 text-base font-light">
-              sum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod
+              There are many interesting experiments that can be done using the
+              Yolo algorithm, such as detecting the height of a river. This blog
+              shares how to deploy the Yolo algorithm program using an Android
+              phone.
             </p>
           </div>
         </div>
